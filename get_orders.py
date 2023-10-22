b314ba5a63c6dcd2f7faae7474397ed9b3b79871
Rene Lafleur <rene.lafleur@coenatek.com>
2023-10-21 22:19:06 -0400

    Remove unused line

--- get_orders.py
+++ get_orders.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import cb_api, json
 
-url = "/api/v3/brokerage/orders/historical/batch"
-
 response = cb_api.get_response('/api/v3/brokerage/orders/historical/batch', {'product_id': 'BTC-USDC', 'order_status': 'OPEN'})
 
 print(json.dumps(response.json(), indent=4))
